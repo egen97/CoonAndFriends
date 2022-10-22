f691c02371f284c5e173540b3d398b5e7af9f7ae
Jonas Willibald Schmid <jonassc@uio.no>
2022-10-22 12:34:35 +0200

    j

--- Trololol.R
+++ Trololol.R
@@ -1,1 +1,31 @@
-sdvsdgsda
+pacman::p_load(googleway, ggmap, tidyverse, sf, mapview)
+
+
+gmaps_key <- "AIzaSyBxhs71v6qXOCXG6wIdy9WXeq94e_QxNHw"
+
+register_google(key = gmaps_key, write = TRUE)
+
+
+a <- c("Donetsk", "Kyiv", "Kharkiv")
+
+
+a <- tibble(town = c("Donetsk", "Kyiv", "Kharkiv")) %>%
+   mutate_geocode(town)
+
+
+
+map <- a %>%
+st_as_sf(coords = c("lon",  "lat")) %>%
+  st_set_crs(4326) %>% select(town)
+
+map$bloggercount <- c(5,10,15)
+map$sentiment <- c("desperate", "happy", "angry")
+
+
+
+m <- mapview(map, zcol= c("sentiment"),  cex = "bloggercount" )
+
+m
+
+
+?st_set_crs
