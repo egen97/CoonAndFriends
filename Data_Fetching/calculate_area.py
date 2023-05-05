1d575ce4644f553b8942f7dfb6b945162f920b0c
Eric G. E. Nilsen <e.g.e.nilsen@stv.uio.no>
2023-05-05 16:53:16 +0200

    Maps, maps, and more maps

---
+++ Data_Fetching/calculate_area.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+import csv
+import numpy as np
+
+# Define the target color and color range (in RGB format)
+target_color = (245, 180, 176)
+color_range = 50
+
+
+# Define the folder containing the images
+folder = "Data/pictures/renamed/"
+
+# Define the path to the output CSV file
+output_file = "Data/pictures/area.csv"
+
+# Open the CSV file for writing
+with open(output_file, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+
+    # Write the header row
+    writer.writerow(["Filename", "Percentage"])
+
+    # Loop over every file in the folder
+    for filename in os.listdir(folder):
+
+        # Make sure the file is an image
+        if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+
+            # Construct the full path to the image
+            filepath = os.path.join(folder, filename)
+
+            # Load the image
+            img = cv2.imread(filepath)
+
+            # Calculate the percentage of the image that is within the color range of the target color
+            lower_bound = np.clip(np.array(target_color) - color_range, 0, 255)
+            upper_bound = np.clip(np.array(target_color) + color_range, 0, 255)
+            mask = cv2.inRange(img, lower_bound, upper_bound)
+            num_pixels = img.shape[0] * img.shape[1]
+            target_pixels = cv2.countNonZero(mask)
+            percentage = (target_pixels / num_pixels) * 100
+
+            # Write the filename and percentage to the CSV file
+            writer.writerow([filename, percentage])
--- Data_Fetching/isw_maps.R
+++ Data_Fetching/isw_maps.R
@@ -43,3 +43,21 @@ library(av)
 
 av_video_images("Data/pictures/movie/isw_interactive.mp4", destdir = "Data/pictures/movie", fps = 1)
 #The pictures are from a screencapture of
+#Deleted every other frame by searching for "*0.jpg OR *2.jpg OR *4.jpg OR *6.jpg OR *8.jpg  " in explorer,
+# and removing those. Think it's better to set fps = 2 tbh, but who knows
+#Those are now in the "corrected" folder
+
+dates <- seq.Date(from = as.Date("2022-2-23"), to = as.Date("2023-2-28"), by = "day")
+
+errors <- as.Date(c("2022-8-2", "2022-8-3", "2022-8-4", "2022-8-5"))
+
+dates <- dates[-which(dates %in% errors)]
+
+filnames <- list.files("Data/pictures/corrected/")
+
+file.rename(
+  paste0("Data/pictures/renamed/", filnames),
+  paste0("Data/pictures/renamed/",as.character(dates), ".jpg")
+  )
+
+
