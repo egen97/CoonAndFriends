cad6238ae6d7444ef84b61df56273f4113cf1e07
Eric G. E. Nilsen <e.g.e.nilsen@stv.uio.no>
2023-05-06 13:33:08 +0200

    Testing the image-analysis script (stackoverflow ftw), added .png to the ignore file, probably should do the same with other image-formats?

--- 1_Data_Fetching/calculate_area_0.01.py
+++ 1_Data_Fetching/calculate_area_0.01.py
@@ -4,12 +4,12 @@ import csv
 import numpy as np
 
 # Define the target color and color range (in RGB format)
-target_color = (176, 180, 245)
-color_range = 20
-
+#target_color = (245,480,176)
+target_color = (176, 180, 245)  # OpenCV uses BGR channel ordring, and not RGB.
+color_range = 20 #50  # range of 20 looks fine (at least for the first image).
 
 # Define the folder containing the images
-folder = "Data/pictures/renamed/"
+folder = "Data/pictures/test/"
 
 # Define the path to the output CSV file
 output_file = "Data/pictures/area.csv"
@@ -33,10 +33,11 @@ with open(output_file, 'w', newline='') as csvfile:
             # Load the image
             img = cv2.imread(filepath)
 
-            # Calculate the percentage of the image that is within the color range of the target color
+            # Calculate the percentage of the image that is within the color range of the target 
             lower_bound = np.clip(np.array(target_color) - color_range, 0, 255)
             upper_bound = np.clip(np.array(target_color) + color_range, 0, 255)
             mask = cv2.inRange(img, lower_bound, upper_bound)
+            cv2.imwrite('mask.png', mask)  # Save mask for testing
             num_pixels = img.shape[0] * img.shape[1]
             target_pixels = cv2.countNonZero(mask)
             percentage = (target_pixels / num_pixels) * 100
