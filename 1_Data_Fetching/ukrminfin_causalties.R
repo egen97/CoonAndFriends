c700459584835184f05ef7f21aed1d428fc212ea
Eric G. E. Nilsen <e.g.e.nilsen@stv.uio.no>
2023-05-09 12:03:44 +0200

    and we got the loss data! <3

--- 1_Data_Fetching/ukrminfin_causalties.R
+++ 1_Data_Fetching/ukrminfin_causalties.R
@@ -1,93 +1,152 @@
 library(peacePHDs)
 library(tidyverse)
+library(rvest)
 
-deaths <- rvest::read_html("https://index.minfin.com.ua/en/russian-invading/casualties/graph.inc.php?months=0&subj=personnel&bydays=1")
-tanks_data <- rvest::read_html("https://index.minfin.com.ua/en/russian-invading/casualties/graph.inc.php?months=0&subj=tanks&bydays=1")
-planes_data <- rvest::read_html("https://index.minfin.com.ua/en/russian-invading/casualties/graph.inc.php?months=0&subj=planes&bydays=1")
 
-ukr_minfin_extractor(deaths, 422)[[1]][1]
+### Download Data ####
 
-causalties <- data.frame("date" = rep(NA, 500), "causalties" = rep(NA, 500))
 
-for (i in 5:422) {
-  causalties$date[i] <- ukr_minfin_extractor(deaths, i)[[1]][1]
-  causalties$causalties[i] <- ukr_minfin_extractor(deaths, i)[[1]][[2]]
 
+base_url <- "https://index.minfin.com.ua/en"
+ukr_url <- function(x){
+  return(paste0(base_url, x))
+}
+
+causalties_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=personnel&bydays=1"))
+tanks_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=tanks&bydays=1"))
+planes_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=planes&bydays=1"))
+armored_veichle_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=armored&bydays=1"))
+cannons_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=cannons&bydays=1"))
+mlrs_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=mlrs&bydays=1"))
+helicopters_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=helicopters&bydays=1"))
+uav_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=uav&bydays=1"))
+cruise_misils_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=missiles&bydays=1"))
+ships_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=ships&bydays=1"))
+cars_cisterns_data <- read_html(ukr_url("/russian-invading/casualties/graph.inc.php?months=0&subj=cars&bydays=1"))
+
+#Did not download "special equipment" and "anti-aircraft warfare" as I am not certain of the definition
+
+
+loss_data <- list(
+  armored_veichle_data,
+  cannons_data,
+  cars_cisterns_data,
+  causalties_data,
+  cruise_misils_data,
+  helicopters_data,
+  mlrs_data,
+  planes_data,
+  ships_data,
+  tanks_data,
+  uav_data
+)
+
+
+
+
+#### clean ####
+
+ukr_df <- function(html, name){
+
+  tempdf <- data.frame("date" = rep(NA, 500), name = rep(NA, 500))
+
+  for (i in 5:422) {
+    tempdf[,1][i] <- ukr_minfin_extractor(html, i)[[1]][1]
+    tempdf[,2][i] <- ukr_minfin_extractor(html, i)[[1]][[2]]
+
+  }
+
+  tempdf <- tempdf %>%
+    drop_na() %>%
+    mutate(across(.cols = c(2), ~ as.numeric(str_remove(.x, "'"))),
+           date = str_remove(date, "'"),
+           date = as.Date(date, tryFormats = c("%d.%m.%Y"))
+    )
+
+  names(tempdf)[2] <- name
+
+  return(tibble(tempdf))
 }
 
 
+types <- c(
+  "armored_veichle",
+"cannons",
+"cars_cisterns",
+"causalties",
+"cruise_misils",
+"helicopters",
+"mlrs",
+"planes",
+"ships",
+"tanks",
+"uav"
+)
 
-causalties <- causalties %>%
-  drop_na() %>%
-  mutate(causalties = as.numeric(str_remove(causalties, "'")),
-        date = str_remove(date, "'"),
-        date = as.Date(date, tryFormats = c("%d.%m.%Y"))
-        )
 
-ggplot(causalties, aes(date, causalties)) +
-  geom_line()
+loss_df <- map2(loss_data, types, possibly(ukr_df))
 
 
-tanks <- data.frame("date" = rep(NA, 500), "tanks_losses" = rep(NA, 500))
 
-for (i in 5:422) {
-  tanks$date[i] <- ukr_minfin_extractor(tanks_data, i)[[1]][1]
-  tanks$tanks_losses[i] <- ukr_minfin_extractor(tanks_data, i)[[1]][[2]]
+purrr::possibly()
+
+#Cruise misiles got an subscript out of bonds error
+cruise_misils <- data.frame("date" = rep(NA, 500), name = rep(NA, 500))
+
+for (i in 5:374) {
+  cruise_misils[,1][i] <- ukr_minfin_extractor(cruise_misils_data, i)[[1]][1]
+  cruise_misils[,2][i] <- ukr_minfin_extractor(cruise_misils_data, i)[[1]][[2]]
 
 }
 
-
-
-tanks <- tanks %>%
+cruise_misils <- cruise_misils %>%
   drop_na() %>%
-  mutate(tanks_losses = as.numeric(str_remove(tanks_losses, "'")),
+  mutate(name = as.numeric(str_remove(name, "'")),
          date = str_remove(date, "'"),
          date = as.Date(date, tryFormats = c("%d.%m.%Y"))
-  )
-
-ggplot(tanks, aes(date, tanks_losses)) +
-  geom_line()
-
-
-planes <- data.frame("date" = rep(NA, 500), "planes_losses" = rep(NA, 500))
-
-for (i in 5:422) {
-  planes$date[i] <- ukr_minfin_extractor(planes_data, i)[[1]][1]
-  planes$planes_losses[i] <- ukr_minfin_extractor(planes_data, i)[[1]][[2]]
-
-}
-
-
-
-planes <- planes %>%
-  drop_na() %>%
-  mutate(planes_losses = as.numeric(str_remove(planes_losses, "'")),
-         date = str_remove(date, "'"),
-         date = as.Date(date, tryFormats = c("%d.%m.%Y"))
-  )
-
-
-causalties %>%
-  left_join(tanks) %>%
-  left_join(planes) %>%
-  pivot_longer(2:4, names_to = "type", values_to = "loss") %>%
-  mutate(type = case_when(
-    type == "causalties" ~ "Casualties",
-    type == "tanks_losses" ~ "Tanks",
-    type == "planes_losses" ~ "Planes"
-    ),
-    loss = ifelse(is.na(loss), 0, loss)
   ) %>%
-  ggplot(aes(date, loss, colour = type)) +
-  geom_line() +
-  geom_smooth(se = FALSE) +
-  facet_wrap(~type, scales = "free_y") +
-  ggthemes::theme_excel_new() +
-  labs(
-    title = "Russian Losses in Ukraine",
-    subtitle = "Collected by the Ukrainin Ministry of Defence",
-    caption = 'Source: Ukranian Ministry of Finance, "Casualties of the Russian troops in Ukraine", https://index.minfin.com.ua/en/russian-invading/casualties/'
-  )
+  rename("cruise_misils" = "name")
+
+loss_df[[5]] <- as_tibble(cruise_misils)
 
 
+merged_df <- loss_df %>%
+  reduce(full_join, by = "date")
+
+merged_df <- merged_df %>%
+  mutate(across(2:ncol(.), ~ifelse(is.na(.x), 0, .x)))
+
+saveRDS(merged_df, "Data/russian_losses.rds")
+
+#### Graphs ####
+merged_df %>%
+  pivot_longer(2:ncol(.), names_to = "type", values_to = "loss") %>%
+    mutate(
+      type =
+        case_when(
+        type == "causalties" ~ "Casualties",
+        type == "tanks" ~ "Tanks",
+        type == "planes" ~ "Planes",
+        type == "armored_veichle" ~ "Armored Vehicle",
+        type == "cannons" ~ "Cannons",
+        type == "cars_cisterns" ~  "Cars and Cisterns",
+        type == "cruise_misils" ~ "Cruise Missile",
+        type == "helicopters" ~ "Helicopters",
+        type == "mlrs" ~ "Multiple Launch Rocket System",
+        type == "ships" ~ "Ships",
+        type == "uav" ~ "Drone"
+      )
+    ) %>%
+    ggplot(aes(date, loss, colour = type)) +
+    geom_line() +
+    geom_smooth(se = FALSE) +
+    facet_wrap(~type, scales = "free_y") +
+    ggthemes::theme_excel_new() +
+    labs(
+      title = "Russian Losses in Ukraine",
+      tag = "Note the differing Y-axis",
+      subtitle = "Collected by the Ukrainin Ministry of Defence",
+      caption = 'Source: Ukranian Ministry of Finance, "Casualties of the Russian troops in Ukraine", https://index.minfin.com.ua/en/russian-invading/casualties/'
+  ) +
+  guides(colour = "none")
 
