b095c9a0927adc147e07a5ac86a3cf1502302d0a
Eric Gabo Ekeberg Nilsen <egnilsen@uio.no>
2026-03-03 12:26:29 +0100

    Made some new descriptives of the independent variabels, all uploaded to github

--- 4_Analyze/descriptives.R
+++ 4_Analyze/descriptives.R
@@ -21,5 +21,48 @@ war_data %>%
     x = ""
   ) +
   theme(
-    axis.title = element_text(size = 14)
+    axis.title = element_text(size = 16),
+    axis.text = element_text(size = 14)
   )
+
+
+
+# Russian Equipment LER over time
+
+war_data %>%
+  ggplot(aes(date, log_equipment_ler_rollmean)) +
+  geom_line(linewidth = 1.3) +
+  geom_hline(aes(yintercept = 0), linewidth = 1.3) +
+  theme_minimal() +
+  labs(
+    y = "Russian Equipment Loss/Exchange ratio: 14-day rolling mean",
+    x = ""
+  ) +
+  theme(
+    axis.title = element_text(size = 16),
+    axis.text = element_text(size = 14)
+  )
+
+
+# Occupied territory over time
+
+
+
+war_data %>%
+  ggplot(aes(date, occupied_rollmean)) +
+  geom_line(linewidth = 1.3) +
+  theme_minimal() +
+  labs(
+    y = "Russian Occupied territory: 14-day rolling mean",
+    x = ""
+  ) +
+  scale_y_continuous(labels = scales::label_percent(scale = 1), limits = c(NA,20))
+  theme(
+    axis.title = element_text(size = 16),
+    axis.text = element_text(size = 14)
+  )
+
+
+
+
+
